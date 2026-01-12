91f294885254ad29fe7ae6227d1d534075a91b78
gela08 <angelajelly.gardan@hcdc.edu.ph>
2026-01-12 12:30:29 +0800

    homepage

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Nav from "./components/Nav"
 import Footer from "./components/Footer"
 import AnimatedBackground from "./components/AnimatedBackground"
 
-import Home from "./pages/Home"
-import Letter from "./pages/Letter"
-import Memories from "./pages/Memories"
-import Gallery from "./pages/Gallery"
-import Final from "./pages/Final"
+// import Home from "./pages/Home"
+// import Letter from "./pages/Letter"
+// import Memories from "./pages/Memories"
+// import Gallery from "./pages/Gallery"
+// import Final from "./pages/Final"
 
 export default function App() {
   return (
